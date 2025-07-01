4901d7e1aa8a88dba8f6a02a4bb657ceed6b100f
Bichngocc07 <ngoc01072006@gmail.com>
2025-07-01 22:29:19 +0700

    Update sesex06.c

--- sesex06.c
+++ sesex06.c
@@ -1,3 +1,75 @@
 //
 // Created by Owner on 7/1/2025.
 //
+#include <stdio.h>
+
+#define MAX 1000
+
+// Merge function
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data into temporary arrays
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the arrays back into arr[]
+    i = 0; j = 0; k = left;
+    while (i < n1 && j < n2)
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+
+    // Copy remaining elements
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+// Merge Sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n;
+    int arr[MAX];
+
+    // Input number of elements
+    printf("Nhap so luong phan tu n: ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n >= 1000) {
+        printf("So luong phan tu khong hop le\n");
+        return 1;
+    }
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Sort the array
+    mergeSort(arr, 0, n - 1);
+
+    // Output the result
+    printf("Mang da sap xep: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
